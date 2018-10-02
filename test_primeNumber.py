65a1523cf8bacfc3bd7d955a11315c3ccece5aaf
sraju728@gmail.com <sraju728@gmail.com>
2018-10-02 16:23:38 +0530

    Adding testcase to check wheather primenumber or not

--- test_primeNumber.py
+++ test_primeNumber.py
@@ -6,3 +6,6 @@ class PrimeNumberTests(unittest.TestCase):
 	
 		def test_NotPrimeNumber(self):
         		self.assertFalse(prime_number(4))
+		
+		def test_PrimeNumber(self):
+        		self.assertTrue(prime_number(2))
