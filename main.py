9c72c425196e19c198f4ebdca2eb224795d0d131
sraju728@gmail.com <sraju728@gmail.com>
2018-10-02 16:29:59 +0530

    Write code to pass the testcase(Zero)

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ def prime_number(number):
 	if number < 0:
         raise ValueError('Input should be positive')
 		
+	if number == 0:
+        return False
+		
 	if number > 1:
         for i in range(2, number):
             if (number % i) == 0:
