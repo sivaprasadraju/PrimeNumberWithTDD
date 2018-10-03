d373ff4fb3ee41203b9f19ecb88bc3c07939d73f
Siva Prasad Raju <30493045+sivaprasadraju@users.noreply.github.com>
2018-10-03 09:20:35 +0530

    Refactor the code

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 def prime_number(number):
 	if number < 0:
-        raise ValueError('Input should be positive')
+        	raise ValueError('Input should be positive')
 		
 	if number == 0:
-        return False
+        	return False
 		
 	if number > 1:
-        for i in range(2, number):
-            if (number % i) == 0:
-                return False
-                break
-        else:
-            return True
+        	for i in range(2, number):
+            		if (number % i) == 0:
+                		return False
+                		break
+        		else:
+            			return True
 
-    else:
-        return False
+    	else:
+        	return False
